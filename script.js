49fd432dd3aa690cbb25ee5e78482d55250374ea
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-05-01 09:36:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+function a() {
+	let name=document.getElementById('name').value;
+	let year=document.getElementById('year').value;
+	let url=`https://localhost:8080/?name=${name}&year=${year}`;
+	let h3=document.getElementById('url').textContext=url
+	
+}
